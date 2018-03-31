88e27f5c9ef4f54433f851e58f044c243dbdf761
romart007 <32412953+romart007@users.noreply.github.com>
2018-03-31 10:25:00 +0800

    styled services and added modal

---
+++ flip.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	$('.btn-read-1').click(function(){
+			$("card-1").addClass("side");
+			$("card-1").addClass("back");
+			$("card-1").addClass("rotate");
+
+	});
+});
